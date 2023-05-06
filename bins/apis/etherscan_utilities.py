85b834541c7bd398dc631a2b39ba22c3a7154a37
iospap <pau@nabbo.com>
2023-05-06 20:06:51 +0200

    get contract creation

--- bins/apis/etherscan_utilities.py
+++ bins/apis/etherscan_utilities.py
@@ -4,7 +4,6 @@ from bins.general import net_utilities
 
 
 class etherscan_helper:
-
     _urls = {
         "ethereum": "https://api.etherscan.io",
         "polygon": "https://api.polygonscan.com",
@@ -53,7 +52,6 @@ class etherscan_helper:
 
     # PUBLIC
     def get_contract_supply(self, network: str, contract_address: str) -> int:
-
         url = "{}/api?{}&apiKey={}".format(
             self._urls[network.lower()],
             self.build_url_arguments(
@@ -71,7 +69,6 @@ class etherscan_helper:
 
         # loop till no more results are retrieved
         while True:
-
             try:
                 url = "{}/api?{}&apiKey={}".format(
                     self._urls[network.lower()],
@@ -143,6 +140,82 @@ class etherscan_helper:
         )
 
         return self._request_data(url)
+
+    def get_contract_creation(
+        self, network: str, contract_addresses: list[str]
+    ) -> list:
+        """_summary_
+
+        Args:
+            network (str): _description_
+            contract_addresses (list[str]): _description_
+
+        Returns:
+            list: [{ contractAddress: "0xb1a0e5fee652348a206d935985ae1e8a9182a245",
+                    contractCreator: "0x71e7d05be74ff748c45402c06a941c822d756dc5",
+                    txHash: "0x4d7e24a6dab8ba46440a8df3cfca8a4e8225fa2d5daf312c21f0647000d6ce42"
+                    }]
+        """
+        result = []
+        page = 1  # define pagination var
+        offset = 10000  # items to be presented with on each query
+        # loop till no more results are retrieved
+        while True:
+            try:
+                url = "{}/api?{}&apiKey={}".format(
+                    self._urls[network.lower()],
+                    self.build_url_arguments(
+                        module="contract",
+                        action="getcontractcreation",
+                        contractaddress=contract_addresses,
+                        page=page,
+                        offset=offset,
+                        sort="asc",
+                    ),
+                    self._api_keys[network.lower()],
+                )
+
+                # rate control
+                self.__RATE_LIMIT.continue_when_safe()
+
+                # get data
+                _data = net_utilities.get_request(
+                    url
+                )  #  {"status":"1","message":"OK-Missing/Invalid API Key, rate limit of 1/5sec applied","result":....}
+
+                if _data["status"] == "1":
+                    # query when thru ok
+                    if _data["result"]:
+                        # Add data to result
+                        result += _data["result"]
+
+                        if len(_data["result"]) < offset:
+                            # there is no more data to be scraped
+                            break
+                        else:
+                            # add pagination var
+                            page += 1
+                    else:
+                        # no data
+                        break
+                else:
+                    logging.getLogger(__name__).debug(
+                        " {} for {} in {}  . error message: {}".format(
+                            _data["message"], contract_addresses, network
+                        )
+                    )
+                    break
+
+            except Exception:
+                # do not continue
+                logging.getLogger(__name__).error(
+                    f' Unexpected error while querying url {url}    . error message: {_data["message"]}'
+                )
+
+                break
+
+        # return result
+        return result
 
     def _request_data(self, url):
         self.__RATE_LIMIT.continue_when_safe()
