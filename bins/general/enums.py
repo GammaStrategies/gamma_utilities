483f9981d2c0f055c70f1e938d55ca7fd8ee7164
iospap <pau@nabbo.com>
2023-05-26 19:48:24 +0200

    prepare enums implem.

--- bins/general/enums.py
+++ bins/general/enums.py
@@ -2,36 +2,97 @@ from enum import Enum, unique
 
 
 class Chain(str, Enum):
-    ARBITRUM = "arbitrum"
-    CELO = "celo"
-    ETHEREUM = "ethereum"
-    OPTIMISM = "optimism"
-    POLYGON = "polygon"
-    BSC = "bsc"
-    POLYGON_ZKEVM = "polygon_zkevm"
-    AVALANCHE = "avalanche"
-    FANTOM = "fantom"
-    MOONBEAM = "moonbeam"
+    #      ( value , id , API url, API name, subgraph name, database name, fantasy_name )
+    ARBITRUM = ("arbitrum", 42161, "arbitrum", "Arbitrum")
+    CELO = ("celo", 42220, "celo", "Celo")
+    ETHEREUM = ("ethereum", 1, "mainnet", "Ethereum")
+    OPTIMISM = ("optimism", 10, "optimism", "Optimism")
+    POLYGON = ("polygon", 137, "polygon", "Polygon")
+    BSC = ("bsc", 56, "binance", "Binance Chain")
+    POLYGON_ZKEVM = (
+        "polygon_zkevm",
+        1101,
+        "polygon_zkevm",
+        "Polygon zkEVM",
+    )
+    AVALANCHE = ("avalanche", 43114, "avalanche", "Avalanche")
+    FANTOM = ("fantom", 250, "fantom", "Fantom")
+    MOONBEAM = ("moonbeam", 1287, "moonbeam", "Moonbeam")
+
+    # extra properties
+    id: int
+    database_name: str
+    fantasy_name: str
+
+    def __new__(
+        self,
+        value: str,
+        id: int,
+        database_name: str | None = None,
+        fantasy_name: str | None = None,
+    ):
+        """_summary_
+
+        Args:
+            value (str): _description_
+            id (int): _description_
+            database_name (str | None, optional): . Defaults to value.
+            fantasy_name (str | None, optional): . Defaults to value.
+
+        Returns:
+            _type_: _description_
+        """
+        obj = str.__new__(self, value)
+        obj._value_ = value
+        obj.id = id
+        # optional properties
+        obj.database_name = database_name or value.lower()
+        obj.fantasy_name = fantasy_name or value.lower()
+        return obj
 
 
-class Dex(str, Enum):
-    QUICKSWAP = "quickswap"
-    UNISWAP = "uniswap"
-    ZYBERSWAP = "zyberswap"
-    THENA = "thena"
-    GLACIER = "glacier"
-    CAMELOT = "camelot"
-    RETRO = "retro"
-    STELLASWAP = "stellaswap"
+class Protocol(str, Enum):
+    #            ( value , url, fantasy_name )
+    GAMMA = ("gamma", "Gamma Strategies")
 
+    ALGEBRAv3 = ("algebrav3", "UniswapV3")
+    UNISWAPv3 = ("uniswapv3", "AlgebraV3")
 
-@unique
-class ChainId(int, Enum):
-    ARBITRUM = 42161
-    CELO = 42220
-    ETHEREUM = 1
-    OPTIMISM = 10
-    POLYGON = 137
-    BSC = 56
-    POLYGON_ZKEVM = 1101
-    AVALANCHE = 43114
+    QUICKSWAP = ("quickswap", "QuickSwap")
+    UNISWAP = ("uniswap", "Uniswap")
+    ZYBERSWAP = ("zyberswap", "Zyberswap")
+    THENA = ("thena", "Thena")
+    GLACIER = ("glacier", "Glacier")
+    SPIRITSWAP = ("spiritswap", "SpiritSwap")
+    CAMELOT = ("camelot", "Camelot")
+    RETRO = ("retro", "Retro")
+    STELLASWAP = ("stellaswap", "Stellaswap")
+    BEAMSWAP = ("beamswap", "Beamswap")
+    RAMSES = ("ramses", "Ramses")
+    VEZARD = ("vezard", "veZard")
+
+    # extra properties
+    database_name: str
+    fantasy_name: str
+
+    def __new__(
+        self,
+        value: str,
+        database_name: str | None = None,
+        fantasy_name: str | None = None,
+    ):
+        """
+
+        Args:
+            value (_type_): chain name
+            id (_type_): chain id
+
+        Returns:
+            : Chain
+        """
+        obj = str.__new__(self, value)
+        obj._value_ = value
+        # optional properties
+        obj.database_name = database_name or value.lower()
+        obj.fantasy_name = fantasy_name or value.lower()
+        return obj
