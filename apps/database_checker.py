30a85f21f817b3afb776f79b420d7ecae0eaa696
iospap <pau@nabbo.com>
2023-07-31 11:28:32 +0200

    disable repair queue failed

--- apps/database_checker.py
+++ apps/database_checker.py
@@ -1502,10 +1502,11 @@ def repair_queue():
     # locked items
     repair_queue_locked_items()
 
+    # TODO: replace with non fixed list lenght solution like the "manual_scrape_from_queue" from  tests
     # try process failed items with count > 10
-    repair_queue_failed_items(
-        count_gte=CONFIGURATION["_custom_"]["cml_parameters"].queue_count or 10
-    )
+    # repair_queue_failed_items(
+    #    count_gte=CONFIGURATION["_custom_"]["cml_parameters"].queue_count or 10
+    # )
 
 
 def repair_queue_locked_items():
