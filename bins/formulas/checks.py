cfa3787daa84a06ecd7183cfba03fb545aaf4312
iospap <pau@nabbo.com>
2023-09-26 18:22:07 +0200

    log check errors

--- bins/formulas/checks.py
+++ bins/formulas/checks.py
@@ -16,69 +16,69 @@ from bins.formulas.constants import (
 
 def checkInt(number):
     if not type(number) == int:
-        raise TypeError("Input must be an integer")
+        raise TypeError(f"Input must be an integer [{number}]")
 
 
 def checkUInt128(number):
     checkInt(number)
     if number < 0 or number > MAX_UINT128:
-        raise ValueError("Input must be between 0 and 2^128 - 1")
+        raise ValueError("Input must be between 0 and 2^128 - 1   [{number}]")
 
 
 def checkInt128(number):
     checkInt(number)
     if number < MIN_INT128 or number > MAX_INT128:
-        raise ValueError("Input must be between -2^127 and 2^127 - 1")
+        raise ValueError("Input must be between -2^127 and 2^127 - 1   [{number}]")
 
 
 def checkInt256(number):
     checkInt(number)
     if number < MIN_INT256 or number > MAX_INT256:
-        raise ValueError("Input must be between -2^255 and 2^255 - 1")
+        raise ValueError("Input must be between -2^255 and 2^255 - 1   [{number}]")
 
 
 def checkUInt160(number):
     checkInt(number)
     if number < 0 or number > MAX_UINT160:
-        raise ValueError("Input must be between 0 and 2^160 - 1")
+        raise ValueError("Input must be between 0 and 2^160 - 1   [{number}]")
 
 
 def checkUInt256(number):
     checkInt(number)
     if number < 0 or number > MAX_UINT256:
-        raise ValueError("Input must be between 0 and 2^256 - 1")
+        raise ValueError("Input must be between 0 and 2^256 - 1   [{number}]")
 
 
 def checkUInt8(number):
     checkInt(number)
     if number < 0 or number > MAX_UINT8:
-        raise ValueError("Input must be between 0 and 2^8 - 1")
+        raise ValueError("Input must be between 0 and 2^8 - 1   [{number}]")
 
 
 def checkInt24(number):
     checkInt(number)
     if number < MIN_INT24 or number > MAX_INT24:
-        raise ValueError("Input must be between -2^23 and 2^23 - 1")
+        raise ValueError("Input must be between -2^23 and 2^23 - 1   [{number}]")
 
 
 def checkfloat(input):
     if type(input) != float:
-        raise TypeError("Input must be a float")
+        raise TypeError("Input must be a float   [{input}]")
 
 
 def checkString(input):
     if type(input) != str:
-        raise TypeError("Input must be a string")
+        raise TypeError("Input must be a string   [{input}]")
 
 
 def checkDecimal(input):
     if type(input) != Decimal:
-        raise TypeError("Input must be a Decimal")
+        raise TypeError("Input must be a Decimal   [{input}]")
 
 
 def checkDict(input):
     if type(input) != dict:
-        raise TypeError("Input must be a dict")
+        raise TypeError("Input must be a dict   [{input}]")
 
 
 def checkAccount(address):
