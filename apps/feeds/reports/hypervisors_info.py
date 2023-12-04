7af1b8be29d43d5a4528063532543056f1bc59b0
iospap <pau@nabbo.com>
2023-12-04 17:50:34 +0100

    add feat

--- apps/feeds/reports/hypervisors_info.py
+++ apps/feeds/reports/hypervisors_info.py
@@ -13,8 +13,11 @@ def get_info_hypervisors(
     max_calls_atOnce: int = 1000,
     block: int = 0,
     timestamp: int = 0,
+    custom_rpcType: str | None = None,
+    hypervisor_fn_names: list[str] | None = None,
+    pool_fn_names: list[str] | None = None,
 ):
-    """Get information using web3 multicall
+    """Get hype+pool information using web3 multicall
 
     Args:
         chain (Chain):
@@ -22,30 +25,18 @@ def get_info_hypervisors(
         max_calls_atOnce (int, optional): . Defaults to 1000.
         block (int, optional): . Defaults to 0.
         timestamp (int, optional): . Defaults to 0.
+        custom_rpcType (str, optional): Force the type of RPC to use: either 'private' or 'public' . Defaults to None.
+        hypervisor_fn_names (list[str], optional): function names to include (read fn only). Defaults to all read functions.
+        pool_fn_names (list[str], optional): function names to include (read fn only) . Defaults to all read functions.
 
     Returns:
         dict:  {<hypervisor_address>:{
                     "objectType": "hypervisor",
-                    "pool": <pool_address>,
-                    "getBasePosition": <getBasePosition>,
-                    "getLimitPosition": <getLimitPosition>,
-                    "getTotalAmounts": <getTotalAmounts>,
-                    "limitLower": <limitLower>,
-                    "limitUpper": <limitUpper>,
-                    "baseLower": <baseLower>,
-                    "baseUpper": <baseUpper>,
-                    "tickSpacing": <tickSpacing>,
-                    "currentTick": <currentTick>,
-                    "decimals": <decimals>,
-                    "token0": <token0_address>,
-                    "token1": <token1_address>,
-                    "fee": <fee>,
-                    "name": <name>,
-                    "symbol": <symbol>,
+                    ...
+                    ...
                 },
                 <pool_address>:{
                     "objectType": "pool",
-
                     ...
                     ...
                 }}
@@ -65,6 +56,7 @@ def get_info_hypervisors(
     block = _erc_dummy_obj.block
     timestamp = _erc_dummy_obj._timestamp
 
+    _pool_addresses_processed = set()
     for _hype in static_hypervisors:
         _hype_dummy_obj = build_hypervisor(
             network=chain.database_name,
@@ -79,6 +71,10 @@ def get_info_hypervisors(
         for fn in _hype_dummy_obj.get_abi_functions(
             types=["function"], stateMutabilitys=["view", "pure"], inputs_exist=False
         ):
+            # filter by function name
+            if hypervisor_fn_names and fn["name"] not in hypervisor_fn_names:
+                continue
+            #
             _calls.append(
                 build_call_with_abi_part(
                     abi_part=fn,
@@ -88,25 +84,38 @@ def get_info_hypervisors(
                 )
             )
 
-        _pool_dummy_obj = build_protocol_pool(
-            chain=chain,
-            protocol=text_to_protocol(_hype["pool"]["dex"]),
-            pool_address=_hype["pool"]["address"],
-            block=block,
-            timestamp=timestamp,
-        )
-        # get all pool's read functions and save em in _calls
-        for fn in _pool_dummy_obj.get_abi_functions(
-            types=["function"], stateMutabilitys=["view", "pure"]
-        ):
-            _calls.append(
-                build_call_with_abi_part(
-                    abi_part=fn,
-                    inputs_values=[],
-                    address=_hype["pool"]["address"],
-                    object="pool",
-                )
+        if _hype["pool"]["address"] not in _pool_addresses_processed:
+            _pool_dummy_obj = build_protocol_pool(
+                chain=chain,
+                protocol=text_to_protocol(_hype["pool"]["dex"]),
+                pool_address=_hype["pool"]["address"],
+                block=block,
+                timestamp=timestamp,
             )
+            # get all pool's read functions and save em in _calls
+            for fn in _pool_dummy_obj.get_abi_functions(
+                types=["function"], stateMutabilitys=["view", "pure"]
+            ):
+                # filter by function name
+                if pool_fn_names and fn["name"] not in pool_fn_names:
+                    continue
+                #
+                _calls.append(
+                    build_call_with_abi_part(
+                        abi_part=fn,
+                        inputs_values=[],
+                        address=_hype["pool"]["address"],
+                        object="pool",
+                    )
+                )
+
+            # add pool address to processed
+            _pool_addresses_processed.add(_hype["pool"]["address"])
+
+    # log qtty of calls to be executed
+    logging.getLogger(__name__).info(
+        f" {chain.fantasy_name}: {len(_calls)} fn calls will be executed for {len(static_hypervisors)} hypervisors, meaning {len(_calls)/len(static_hypervisors):.1f} fn calls per hype, that will be splited in {len(_calls)/max_calls_atOnce:,.0f} batches of {max_calls_atOnce} function calls, for each web3 call to RPC."
+    )
 
     # place all previously build calls
     hypervisors_info = []
@@ -117,6 +126,7 @@ def get_info_hypervisors(
             calls=_calls[i : i + max_calls_atOnce],
             convert_bint=False,
             requireSuccess=False,
+            custom_rpcType=custom_rpcType,
         )
 
     # parse results to an intelligible structure
@@ -132,10 +142,12 @@ def get_info_hypervisors(
             result[_address]["timestamp"] = timestamp
 
         if _fn_name in result[_address]:
-            # error
-            logging.getLogger(__name__).error(
-                f" function {_fn_name} result is duplicated for {_address}. Maybe ABI err?"
-            )
+            # multiple hypes have common pools
+            if hinfo["object"] != "pool":
+                # error
+                logging.getLogger(__name__).error(
+                    f" function {_fn_name} result is duplicated for {_address}: {result[_address]}. Maybe ABI err?"
+                )
 
         # build field values
         # check if all outputs have something in the "name" key.
@@ -153,7 +165,7 @@ def get_info_hypervisors(
             if not "value" in output:
                 # error
                 logging.getLogger(__name__).error(
-                    f" hype {hinfo['address']} has no result on {output['name']}"
+                    f" hype {hinfo['address']} has no result on {_fn_name}>{output['name'] or output}"
                 )
             else:
                 if isinstance(result[_address][_fn_name], str):
