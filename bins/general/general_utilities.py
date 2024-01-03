b0b829e7450396b933da834ecf4f236ad35c5be1
iospap <pau@nabbo.com>
2024-01-03 09:33:43 +0100

    new helper func

--- bins/general/general_utilities.py
+++ bins/general/general_utilities.py
@@ -8,6 +8,7 @@ import datetime as dt
 from pathlib import Path
 from typing import Iterable, Any, Tuple
 from dateutil.relativedelta import relativedelta
+from collections.abc import MutableMapping
 
 log = getLogger(__name__)
 
@@ -482,3 +483,17 @@ def get_last_timestamp_of_day(timestamp: int, tz: dt.timezone | None = None) -> 
         + relativedelta(days=1)
         - relativedelta(seconds=1)
     ).timestamp()
+
+
+# DICT CONVERSION
+def flatten_dict(
+    d: MutableMapping, parent_key: str = "", sep: str = "."
+) -> MutableMapping:
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
