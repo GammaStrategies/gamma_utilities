600d13196e01439cdee23a98e71b421d61fd6431
iospap <pau@nabbo.com>
2024-03-14 13:28:55 +0100

    new info

--- apps/checks/telegram_special/prices.py
+++ apps/checks/telegram_special/prices.py
@@ -14,11 +14,18 @@ def telegram_checks_tokens_without_price(chains: list[Chain] | None = None):
         chains (list[Chain] | None, optional): list of chains to process. Defaults to All.
 
     """
-
+    total = {
+        "tokens": 0,
+        "chains": 0,
+        "tokens_without_price": 0,
+    }
     chains = chains or list(Chain)
 
     with tqdm.tqdm(total=len(chains)) as progress_bar:
         for chain in chains:
+            # add to total chains
+            total["chains"] += 1
+
             # get tokens from static collection
             static_hypes = get_from_localdb(
                 network=chain.database_name, collection="static", find={}
@@ -46,6 +53,9 @@ def telegram_checks_tokens_without_price(chains: list[Chain] | None = None):
             price_helper = price_scraper(thegraph=False)
 
             for token_address, token_symbol in tokens.items():
+                # add to total tokens
+                total["tokens"] += 1
+
                 try:
                     _tmpPrice, _tmpSource = price_helper.get_price(
                         network=chain.database_name,
@@ -55,13 +65,15 @@ def telegram_checks_tokens_without_price(chains: list[Chain] | None = None):
                     if not _tmpPrice:
                         send_to_telegram.error(
                             msg=[
-                                f"<b>\n {chain.fantasy_name} {token_symbol} is not getting the price correctly </b>",
+                                f"<b>\n {chain.fantasy_name} {token_symbol} token is not getting the price correctly </b>",
                                 f"<pre>{token_address}</pre>",
                                 f" ",
                             ],
                             topic="prices",
                             dtime=True,
                         )
+                        # add to total tokens without price
+                        total["tokens_without_price"] += 1
 
                 except Exception as e:
                     logging.getLogger(__name__).exception(
@@ -73,3 +85,15 @@ def telegram_checks_tokens_without_price(chains: list[Chain] | None = None):
                 )
                 progress_bar.refresh()
             progress_bar.update(1)
+
+    # send summary conclusion
+    send_to_telegram.info(
+        msg=[
+            f"<b> Token price check summary:</b>",
+            f"<i> processed</i> <b> chains:</b> {total['chains']:,.0f}",
+            f"<i> processed</i> <b> tokens:</b> {total['tokens']:,.0f}",
+            f"<b> found tokens without price:</b> {total['tokens_without_price']:,.0f}",
+        ],
+        topic="prices",
+        dtime=True,
+    )
